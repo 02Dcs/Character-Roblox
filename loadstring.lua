fdf0044dc7b5550873e165a342426a123ab9fb33
02Dcs <95237577+02Dcs@users.noreply.github.com>
2024-12-21 16:09:11 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Character-Roblox/refs/heads/main/bypass.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Character-Roblox/refs/heads/main/characterai.lua", true))()
